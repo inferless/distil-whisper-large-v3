7290fe2e5c42cb6d448f4f34485efc5a865d8310
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 13:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"]='1'
+from huggingface_hub import snapshot_download
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 class InferlessPythonModel:
     def initialize(self):
-
         model_id = "distil-whisper/distil-large-v3"
-        model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, use_safetensors=True)
-        model.to("cuda:0")
+        snapshot_download(repo_id=model_id,allow_patterns=["*.safetensors"])
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, use_safetensors=True).to("cuda:0")
         processor = AutoProcessor.from_pretrained(model_id)
         self.pipe = pipeline( "automatic-speech-recognition", model=model, tokenizer=processor.tokenizer, feature_extractor=processor.feature_extractor, max_new_tokens=128, torch_dtype=torch.float16, device="cuda:0", )
 
